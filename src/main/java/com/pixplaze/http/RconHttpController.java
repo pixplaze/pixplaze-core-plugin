cdce8e12945081b63cb06f3b0958f8d261be1439
pazik98 <nefedow98@mail.ru>
2022-06-17 16:02:27 +0300

    Измнена обработка запроса `/rcon/lines`.
    	-	Метод `handleLinesRequest` переименован в `handleGetRconLines`.
    	-	Коды ответов теперь вызываются через enum `HttpStatus`.
    	-	Тело ответа в `handleGetRconLines` теперь генерируется в отдельном методе.
    	-	Теперь если параметр count равен нулю, возвращается максимальное количество строк.
    	-	Добавлены комментарии в метод `handleGetRconLines`.

--- src/main/java/com/pixplaze/http/RconHttpController.java
+++ src/main/java/com/pixplaze/http/RconHttpController.java
@@ -12,6 +12,7 @@ import com.pixplaze.util.Utils;
 
 import com.sun.net.httpserver.HttpExchange;
 
+import org.apache.commons.lang.math.NumberUtils;
 import org.bukkit.command.CommandSender;
 import org.bukkit.scheduler.BukkitRunnable;
 
@@ -25,6 +26,7 @@ public class RconHttpController implements HttpController {
 
 	private final PixplazeRootsAPI plugin;
 	private final Logger logger;
+	private final Gson gson = new Gson();
 
 	public RconHttpController(PixplazeRootsAPI plugin, Logger logger) {
 		this.plugin = plugin;
@@ -37,49 +39,50 @@ public class RconHttpController implements HttpController {
 	}
 
 	@GetHandler("/rcon/lines")
-	public void handleLinesRequest(HttpExchange exchange, QueryParams params) throws IOException {
+	public void handleGetRconLines(HttpExchange exchange, QueryParams params) throws IOException {
 		final var MAX_LINES_COUNT = plugin.getConsoleBuffer().getSize();
 		ResponseBodyBuilder rb = new ResponseBodyBuilder();
 
+		// Проверка отправленного токена
 		if (!params.has("access-token")) {
-			sendResponse(exchange, 401, rb.setError("InvalidTokenError").setMessage("No token!").getFinal());
+			rb.setError("InvalidTokenError").setMessage("Token is required");
+			sendResponse(exchange, HttpStatus.UNAUTHORIZED.getCode(), rb.getFinal());
 			return;
 		} else if (!Utils.checkToken(params.getAsString("access-token"))) {
-			sendResponse(exchange, 401, rb.setError("InvalidTokenError").setMessage("Access token is invalid").getFinal());
+			rb.setError("InvalidTokenError").setMessage("Access token is invalid");
+			sendResponse(exchange, HttpStatus.UNAUTHORIZED.getCode(), rb.getFinal());
 			return;
 		}
 
+		/*
+		 * Получение количества возвращаемых строчек консоли.
+		 *
+		 * Если это количество не было указано в запросе или равняется нулю, то запрос вернёт
+		 * максимальное количество строк.
+		 */
 		int count = 0;
 		if (!params.has("count")) {
+			count = MAX_LINES_COUNT;
+		} else if (!NumberUtils.isNumber(params.getAsString("count"))) {
+			rb.setError("InvalidCountError").setMessage("Parameter count must be number!");
+			sendResponse(exchange, HttpStatus.BAD_REQUEST.getCode(), rb.getFinal());
+		} else if (params.getAsInt("count") == 0) {
 			count = MAX_LINES_COUNT;
 		} else {
 			count = params.getAsInt("count");
 		}
 
-		if (count == 0) {
-			sendResponse(exchange, 401, rb.setError("ZeroCountError").setMessage("Param count cannot be zero").getFinal());
-			return;
-		}
-
 		List<String> lines = PixplazeRootsAPI.getInstance().getConsoleBuffer().getHistory(count);
 
+		// Попытка отправки результата запроса
 		try {
-			// fixme
-			Gson gson = new Gson();
-			JsonObject jsonResponse = new JsonObject();
-
-			JsonArray jsonLines = gson.toJsonTree(lines).getAsJsonArray();
-
-			jsonResponse.add("lines", jsonLines);
-			//fixme
-
-			rb.setResponse(jsonResponse).setMessage("Command request success");
-			sendResponse(exchange, 200, rb.getFinal());
+			rb.setResponse(linesToJsonResponse(lines)).setMessage("Command request success");
+			sendResponse(exchange, HttpStatus.OK.getCode(), rb.getFinal());
 		} catch (Exception e) {
 			logger.warning(e.getMessage());
-			sendResponse(exchange, 500, rb.setError(e.getClass().getTypeName()).setMessage(e.getMessage()).getFinal());
+			rb.setError(e.getClass().getTypeName()).setMessage(e.getMessage());
+			sendResponse(exchange, HttpStatus.INTERNAL_ERROR.getCode(), rb.getFinal());
 		}
-
 	}
 
 	@PostHandler("/rcon/command")
@@ -129,6 +132,13 @@ public class RconHttpController implements HttpController {
 		outputStream.flush();
 		outputStream.close();
 		exchange.close();
+	}
+
+	private JsonObject linesToJsonResponse(List<String> lines) {
+		JsonObject jsonResponse = new JsonObject();
+		JsonArray jsonLines = gson.toJsonTree(lines).getAsJsonArray();
+		jsonResponse.add("lines", jsonLines);
+		return jsonResponse;
 	}
 
 	private void dispatchCommand(CommandSender sender, String command) {
