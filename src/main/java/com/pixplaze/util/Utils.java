ebd10ecde8b7dc9b51f45745b77a14e42e1dbd90
pazik98 <nefedow98@mail.ru>
2023-11-22 18:38:34 +0300

    Реализован метод для полчения списка файлов всех плагинов

--- src/main/java/com/pixplaze/util/Utils.java
+++ src/main/java/com/pixplaze/util/Utils.java
@@ -1,10 +1,22 @@
 package com.pixplaze.util;
 
 import com.pixplaze.plugin.PixplazeCorePlugin;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.Plugin;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.List;
 
 public class Utils {
 
     public static boolean checkToken(String token) {
         return token.equals(PixplazeCorePlugin.getInstance().getConfig().getString("access-token"));
+    }
+
+    public static List<File> getPlugins() {
+        return Arrays.stream(Bukkit.getPluginManager().getPlugins())
+                .map(Plugin::getDataFolder)
+                .toList();
     }
 }
